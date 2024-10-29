a6560ec4cf65e61fbd661dc86ecb30ba7655a24f
eb0687 <ebrahim0687@gmail.com>
2024-10-29 19:08:39 +0300

    stuff

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,1 +1,13 @@
 package middlewares
+
+import (
+	"net/http"
+)
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		println("1")
+		next.ServeHTTP(w, r)
+		println("2")
+	})
+}
