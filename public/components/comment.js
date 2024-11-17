dc482f3e18ff41c9fac1c836de2469e731038578
eb0687 <ebrahim0687@gmail.com>
2024-11-17 20:57:22 +0300

    chore: add css to comments (not finished)

--- public/components/comment.js
+++ public/components/comment.js
@@ -4,27 +4,38 @@
  */
 export const Comment = (comment) => {
   return /*html*/ `
-    <div id="comment-container" class="pb-10px">
-        <div class="comment-content" id="comment-${comment.id}">
-            <p>body: ${comment.body}</p>
-            <p>id: ${comment.id}</p>
-            <p>created: ${comment.created_at.Time}</p>
-            <p>userid: ${comment.userid}</p>
-            <p>postid: ${comment.postid}</p>
-            <button class="edit-comment" data-comment-id="${comment.id}">Edit</button>
-            <button class="delete-comment" data-comment-id="${comment.id}">Delete</button>
+    <div class="comment-content" id="comment-${comment.id}">
+      <div id="comment-container" class="pb-10px flex flex-row justify-between">
+        <div id="comment-details-container" class="pr-10px">
+          <p>id: ${comment.id}</p>
+          <p>created: ${comment.created_at.Time}</p>
+          <p>userid: ${comment.userid}</p>
+          <p>postid: ${comment.postid}</p>
+        </div>
+        <div id="comment-body-container" class="flex w-full pt-10px pl-10px">
+          ${comment.body}
+        </div>
+        <div id="comment-buttons-container">
+          <button class="edit-comment" data-comment-id="${comment.id}">Edit</button>
+          <button class="delete-comment" data-comment-id="${comment.id}">Delete</button>
         </div>
         <div class="edit-form" id="edit-form-${comment.id}" style="display: none;">
-            <textarea class="edit-textarea" id="edit-textarea-${comment.id}">${comment.body}</textarea>
-            <button class="save-comment" data-comment-id="${comment.id}">Save</button>
-            <button class="cancel-edit" data-comment-id="${comment.id}">Cancel</button>
+          <textarea class="edit-textarea" id="edit-textarea-${comment.id}">${comment.body}</textarea>
+          <button class="save-comment" data-comment-id="${comment.id}">Save</button>
+          <button class="cancel-edit" data-comment-id="${comment.id}">Cancel</button>
         </div>
+      </div>
     </div>
-    <style>
-      #comment-container{
-        border-bottom: 1px dotted white
-      }
-    </style>
+<style>
+  #comment-container{
+    border-bottom: 1px dotted white
+  }
+  #comment-details-container{
+    border-right: 1px dotted white
+  }
+</style>
+
+
     `;
 };
 
