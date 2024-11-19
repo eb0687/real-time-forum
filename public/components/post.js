e5349860e728cb9d06786915a317f4b46a1ae4c2
eb0687 <ebrahim0687@gmail.com>
2024-11-19 12:40:13 +0300

    chore: add css to homepage

--- public/components/post.js
+++ public/components/post.js
@@ -1,24 +1,75 @@
 /**
- * @param {import("../js/types").Post} post 
+ * @param {import("../js/types").Post} post
  * @returns {string}
  */
 export const Post = (post) => {
-    return /*html*/`
-    <a id="post-${post.id}" >
-        <a href="/posts/${post.id}">${post.title}</a>
-        <p>${post.body}</p>
-    </a>
-    `;
-}
+  return /*html*/ `
 
+<div id="post-container" class="b-3px-border p-10px rounded">
+  <a id="post-${post.id}">
+    <div id="post-title-container" class="flex flex-row justify-start w-100% items-center">
+      <i class="fa-sharp fa-solid fa-comment"></i>
+      <a class="post-title" href="/posts/${post.id}">${post.title}</a>
+    </div>
+    <p class="post-body">
+      ${post.body}
+    </p>
+  </a>
+</div>
+
+
+
+<style>
+  #post-container {
+    flex: 1 1 100px;
+    min-width: 300px;
+    min-height: 300px;
+    border: solid white;
+    transition: background-color 0.3s ease;
+  }
+  #post-container:hover {
+    background-color: #2b2f31;
+  }
+  #post-title-container {
+    text-align-center;
+    border-bottom: 1px solid white
+  } 
+  #post-container:hover #post-title-container {
+    border-bottom-color: black;
+  }
+  .post-title {
+    padding: 10px;
+    font-size: 1.5rem;
+    font-weight: bold;
+    min-height: 50px;
+    text-decoration: none;
+    color: white;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    transition: border-color 0.3s ease;
+  }
+  .post-title:hover {
+    color: #ddd;
+  }
+
+  .post-body {
+    padding: 10px;
+    font-size: 1.2rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+</style>
+
+    `;
+};
 
 /**
- * 
- * @param {import("../js/types").Post[]} posts 
+ *
+ * @param {import("../js/types").Post[]} posts
  * @returns {string}
  */
 export const PostList = (posts) => {
-    if (!posts) return "";
-    return posts.map(Post).join("");
-}
-
+  if (!posts) return "";
+  return posts.map(Post).join("");
+};
--- public/pages/home.js
+++ public/pages/home.js
@@ -3,22 +3,32 @@ import { SpecialFetch } from "../js/utils.js";
 import { attachBaseLayout } from "./layouts.js";
 
 export async function homePage() {
-    const res = await SpecialFetch("/api/posts");
-    if (!res.ok) return;
-    /**
-     * @type {import("../js/types").Post[]}
-     */
-    const posts = await res.json();
+  const res = await SpecialFetch("/api/posts");
+  if (!res.ok) return;
+  /**
+   * @type {import("../js/types").Post[]}
+   */
+  const posts = await res.json();
 
-    await attachBaseLayout(/*html*/`
-        <div>
-            ${PostList(posts)}
-        </div>
-    `, capabilities);
+  await attachBaseLayout(
+    /*html*/ `
+<div id="home-header" class="flex flex-col justify-center items-center p-10px">
+  <h1>Real time forum (placeholder)</h1>
+  <p>Some text describing the project goes here</p>
+</div>
+<div id="main-posts-container" class="flex flex-row gap-40px justify-between pl-120px pr-120px pt-50px pb-50px">
+  ${PostList(posts)}
+</div>
+
+<style>
+  #main-posts-container {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>
+    `,
+    capabilities,
+  );
 }
 
-function capabilities() {
-}
-
-
-
+function capabilities() {}
